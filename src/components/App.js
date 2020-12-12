b1c55d00db5c13628a8a1ec581c64011a3e51818
HariKishore7 <harikishoret99@gmail.com>
2020-12-12 22:25:20 +0530

    Changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+
   return (
-    <></>
-  )
-}
-
+    <div>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
+      <button
+        data-testid="button-restart"
+        disabled={index === 0}
+        onClick={() => setIndex(0)}
+      >
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        disabled={index === 0}
+        onClick={() => {
+          if (index > 0) setIndex(index - 1);
+        }}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={index === props.slides.length - 1}
+        onClick={() => {
+          if (index < props.slides.length - 1) setIndex(index + 1);
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default App;
